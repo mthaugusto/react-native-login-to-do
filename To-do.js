598abfb292d751035efe05f485b5b16b3e921e0f
mthaugusto <mthaugusto94@icloud.com>
2024-02-26 16:50:27 -0300

    Função editar tarefa adicionada e mudanças no design

--- To-do.js
+++ To-do.js
@@ -1,137 +1,261 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, FlatList, TextInput, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  Modal,
+  Portal,
+  Button,
+  Provider as PaperProvider,
+} from "react-native-paper";
 
 export default function Todo() {
-  const [texto, setTexto] = useState('');
+  const [texto, setTexto] = useState("");
   const [tarefa, setTarefa] = useState([]);
   const [idCounter, setIdCounter] = useState(0);
   const [estaFinalizado, setFinalizado] = useState(false);
+  const [textoEditado, setTextoEditado] = useState("");
+  const [editandoId, setEditandoId] = useState(null);
+  const [visible, setVisible] = useState(false);
 
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
 
-  const renderItem = ( {item} ) => {
-           
-      return (
+  const renderItem = ({ item }) => {
+    return (
       <View style={styles.divItem}>
-        <Text style={[styles.itemLista, item.status && { textDecorationLine: 'line-through'}]}>
+        <Text
+          style={[
+            styles.itemLista,
+            item.status && { textDecorationLine: "line-through" },
+          ]}
+        >
           {item.texto}
         </Text>
         <View style={styles.divIcons}>
-        <TouchableOpacity onPress={() => deletarTarefa(item.id)}>
-          <Ionicons name="trash" size={24} color="red" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => concluirTarefa(item.id)}>
-          <Ionicons name={item.status ? "checkmark-circle" : "ellipse"} size={24} color="green" />
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => editarTarefa(item.id)}>
+            <Ionicons name="create" size={24} color="purple" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => deletarTarefa(item.id)}>
+            <Ionicons name="trash" size={24} color="red" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => concluirTarefa(item.id)}>
+            <Ionicons
+              name={item.status ? "checkmark-circle" : "ellipse"}
+              size={24}
+              color="#a6f1a6"
+            />
+          </TouchableOpacity>
         </View>
       </View>
-  )};
+    );
+  };
 
   const addTarefa = () => {
-
-    if (texto === '') {
-      return alert('Digite uma tarefa');
+    if (texto === "") {
+      return alert("Digite uma tarefa");
     } else {
-
-      const novaTarefa = { id: idCounter, texto: texto, status: estaFinalizado };
-      
+      const novaTarefa = {
+        id: idCounter,
+        texto: texto,
+        status: estaFinalizado,
+      };
       setTarefa([...tarefa, novaTarefa]);
-      setTexto('');
+      setTexto("");
       setIdCounter(idCounter + 1);
     }
   };
 
   const deletarTarefa = (id) => {
-          setTarefa(tarefa.filter(item => item.id !== id ))
+    setTarefa(tarefa.filter((item) => item.id !== id));
   };
 
-  // concluido - procurar sobre o map depos
   const concluirTarefa = (id) => {
-          for (i = 0; i < tarefa.length; i++){
-          if (tarefa[i].id === id){
-              tarefa[i].status = !tarefa[i].status
-              setTarefa([...tarefa])
-              break;
-          }
-          }
+    setTarefa(
+      tarefa.map((item) =>
+        item.id === id ? { ...item, status: !item.status } : item
+      )
+    );
   };
-  
+
+  const editarTarefa = (id) => {
+    showModal();
+    setTextoEditado(tarefa[id].texto);
+    setEditandoId(id);
+  };
+
+  const confirmarEdicaoTarefa = () => {
+    if (textoEditado === "") {
+      return alert("Digite uma tarefa");
+    } else {
+      for (let i = 0; i < tarefa.length; i++) {
+        if (tarefa[i].id === editandoId) {
+          tarefa[i].texto = textoEditado;
+          break;
+        }
+      }
+      setTarefa([...tarefa]); // Atualizando a lista de tarefas
+      setTextoEditado("");
+      setEditandoId(null);
+      hideModal();
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tarefas do dia</Text>
-      <FlatList
-        data={tarefa}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite uma tarefa"
-          value={texto}
-          onChangeText={setTexto}
+    <ImageBackground
+      source={require("./assets/background-login.webp")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <PaperProvider>
+          <Text style={styles.greeting}>Olá, usuário</Text>
+          <Text style={styles.subtitle}>O que você tem que fazer hoje?</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Digite uma tarefa"
+              value={texto}
+              onChangeText={setTexto}
+            />
+            <TouchableOpacity style={styles.addBtn} onPress={addTarefa}>
+              <Ionicons name="add" size={30} color="#C0C0C0" />
+            </TouchableOpacity>
+          </View>
+
+          <Text style={styles.taskListTitle}>Sua lista de tarefas:</Text>
+          <FlatList
+            data={tarefa}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
           />
-        <TouchableOpacity
-          style={styles.addBtn} 
-          onPress={addTarefa}>
-          <Ionicons name="add" size={30} color="#C0C0C0" />
-        </TouchableOpacity>
+
+          <Portal>
+            <Modal
+              visible={visible}
+              onDismiss={hideModal}
+              contentContainerStyle={{
+                backgroundColor: "white",
+                alignItems: "center",
+                padding: 50,
+              }}
+            >
+              <TextInput
+                style={styles.inputModal}
+                placeholder="Editar tarefa"
+                value={textoEditado}
+                onChangeText={setTextoEditado}
+              />
+              <TouchableOpacity
+                style={styles.botaoModal}
+                onPress={confirmarEdicaoTarefa}
+              >
+                <Text style={styles.textoBotaoModal}>Confirmar</Text>
+              </TouchableOpacity>
+            </Modal>
+          </Portal>
+        </PaperProvider>
       </View>
-    </View>
+    </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
+    backgroundColor: "rgba(255,255,255,0.5)",
     padding: 20,
+    paddingTop: 100,
   },
-  title: {
+  greeting: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+    color: "black",
+  },
+  subtitle: {
+    fontSize: 18,
+    textAlign: "center",
     marginBottom: 20,
-    paddingTop: 85
+    color: "black",
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 20,
   },
   input: {
     flex: 1,
     height: 40,
     borderRadius: 8,
-    borderColor: 'gray',
+    borderColor: "gray",
     paddingHorizontal: 10,
-    backgroundColor: 'white',
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+  },
+  taskInput: {
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
   },
   addBtn: {
     marginLeft: 10,
     borderRadius: 50,
     padding: 5,
-    backgroundColor: 'white',
+    backgroundColor: "white",
+  },
+  taskListTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
   divItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 15,
     marginBottom: 5,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#ccc',
+    // borderWidth: 1,
+    // borderColor: "gray"
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
   },
   itemLista: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   divIcons: {
-    flexDirection: 'row',
-    paddingLeft: 5,
-  }
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  botaoModal: {
+    borderRadius: 15,
+    backgroundColor: "purple",
+  },
+  textoBotaoModal: {
+    padding: 12,
+    color: "white",
+    fontWeight: "bold",
+  },
+  inputModal: {
+    width: "100%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
 });
